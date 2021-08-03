5be73328f24703a261ea1b67aa3dcda40b372f08
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-03 12:41:41 +0200

    Ex2Task3JS - styled a div tag in JS

--- exercise2/main.js
+++ exercise2/main.js
@@ -1,4 +1,13 @@
+const div = document.createElement("div");
+let size = 0;
+
 document.body.style.height = "10000px";
 
-const div = document.createElement("div");
+div.style.position = "fixed";
+div.style.top = "0";
+div.style.left = "0";
+div.style.width = "100px";
+div.style.heigh = size + "px";
+div.style.backgroundColor = "green";
+
 document.body.appendChild(div);
